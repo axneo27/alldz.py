e00c2764f0b22a9add1657db269343866db65ef3
axneo27 <163739774+axneo27@users.noreply.github.com>
2024-08-23 12:03:58 +0300

    user_private.py

--- TELEGRAMbot/handlers/user_private.py
+++ TELEGRAMbot/handlers/user_private.py
@@ -2,6 +2,8 @@ from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from aiogram import F, Router
 import random
+from HG.goodbyes import goodbyes
+from HG.hellos import hellos
 
 router = Router()
 
@@ -15,12 +17,18 @@ async def cmd_help(message: Message):
 
 @router.message(F.photo)
 async def get_photo(message: Message):
-    await message.reply("This is some photo. So how would it help you to solve the Navier-Stokes equations?")
+    await message.reply("This is some photo.")
 
-hellos = ['Hello', 'Hi', 'Yo', 'Sunny Init']
 for i in range(len(hellos)):
-    if F.text==hellos[i]:
-        @router.message(F.text== hellos[i])
+    if F.text==hellos[i].lower():
+        @router.message(F.text== hellos[i].lower())
         async def hello(message: Message):
             a = random.randrange(0, len(hellos))
-            await message.answer(hellos[a]) 
+            await message.answer(hellos[a])
+
+for j in range(len(goodbyes)):
+    if F.text==goodbyes[j].lower():
+        @router.message(F.text== goodbyes[j].lower())
+        async def bye(message: Message):
+            a = random.randrange(0, len(goodbyes))
+            await message.answer(goodbyes[a])
